1868598398e2cc70c8d562ee64448c5bed27b828
Ryan Yan <rcyan@email.wm.edu>
2019-08-13 17:29:03 -0400

    added build for prod deployement

---
+++ client/build/precache-manifest.19949e13f2bedca9530f40125746df01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3b500b84aafb49f946b9ca1683ec46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f60fa2598b340d6d1a1",
+    "url": "/static/css/main.3efde903.chunk.css"
+  },
+  {
+    "revision": "7ec842e717ec1eefb1c5",
+    "url": "/static/js/2.e698ea7c.chunk.js"
+  },
+  {
+    "revision": "7f60fa2598b340d6d1a1",
+    "url": "/static/js/main.da9db976.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f58b053dfd2e08f6b86c501ccb54d0cb",
+    "url": "/static/media/background-smoke.f58b053d.jpg"
+  }
+]);
